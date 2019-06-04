36f41b51e999f46900e9bb50f9bf422c27263e48
danielobrist <daniel.obrist@students.fhnw.ch>
2019-06-04 10:19:16 +0200

    initial

---
+++ server/src/main/java/ch/fhnw/kvanc/server/integration/QuestionWatchService.java
@@ -0,0 +1,111 @@
+package ch.fhnw.kvanc.server.integration;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import ch.fhnw.kvanc.server.repository.VoteRepository;
+import ch.fhnw.kvanc.server.service.AuthorizationService;
+import ch.fhnw.kvanc.server.web.WebSocketEndpoint;
+
+/**
+ * WatchService
+ */
+@Component
+public class QuestionWatchService {
+    private Logger logger = LoggerFactory.getLogger(QuestionWatchService.class);
+
+    @Value("${watchservice.file:question.txt}")
+    private String filename;
+
+    @Value("${watchservice.path:.}")
+    private String pathname;
+
+    @Autowired
+    private MQTTClient mqttClient;
+
+    @Autowired
+    private VoteRepository voteRepository;
+
+    @Autowired
+    private AuthorizationService authorizationService;
+
+    @Autowired
+    private WebSocketEndpoint websocket;
+
+    private WatchService watchService;
+
+    private Path path;
+
+    private String questionContent = null;
+
+    @PostConstruct
+    public void afterPropertiesSet() throws IOException, InterruptedException {
+        watchService = FileSystems.getDefault().newWatchService();
+        path = Paths.get(pathname);
+        path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+        logger.info("Watching file '{}/{}'", pathname, filename);
+    }
+
+    @Scheduled(fixedRate = 100)
+    public void watch() throws IOException, InterruptedException {
+        WatchKey key = watchService.poll();
+        if (key != null) {
+            for (WatchEvent<?> event : key.pollEvents()) {
+                final Path changed = (Path) event.context();
+                if (changed.endsWith(filename)) {
+                    logger.info("File '{}' has changed", filename);
+                    readFile(pathname + "/" + filename);
+                    if (questionContent.length() > 0) {
+                        logger.info("File content is '{}'", questionContent);
+                    }
+                }
+            }
+            key.reset();
+        }
+    }
+
+    private void readFile(String filepath) throws IOException {
+        File file = new FileSystemResource(filepath).getFile();
+        questionContent = new String(Files.readAllBytes(file.toPath())).trim();
+        if (questionContent.length() == 0) {
+            // reset everything
+            voteRepository.reset();
+            authorizationService.reset();
+            websocket.reset();
+            mqttClient.reset();
+            logger.info("Reset all services");
+        } else {
+            authorizationService.reset();
+            mqttClient.reset();
+            voteRepository.reOpenAll();
+            websocket.pushMessage(questionContent);
+            logger.info("New Question has arrived: '{}'", questionContent);
+        }
+    }
+
+    /**
+     * @return the questionContent
+     */
+    public String getQuestionContent() {
+        return questionContent;
+    }
+
+}
---
+++ server/src/main/java/ch/fhnw/kvanc/server/web/VoteController.java
@@ -0,0 +1,102 @@
+package ch.fhnw.kvanc.server.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.fhnw.kvanc.server.domain.Vote;
+import ch.fhnw.kvanc.server.integration.MQTTClient;
+import ch.fhnw.kvanc.server.integration.QuestionWatchService;
+import ch.fhnw.kvanc.server.repository.VoteRepository;
+import ch.fhnw.kvanc.server.service.AuthorizationService;
+
+@RestController
+@RequestMapping("/votes")
+public class VoteController {
+    private Logger logger = LoggerFactory.getLogger(VoteController.class);
+
+    @Autowired
+    private QuestionWatchService watchService;
+
+    @Autowired
+    private AuthorizationService authorizationService;
+
+    @Autowired
+    private VoteRepository voteRepository;
+
+    @Autowired
+    private MQTTClient mqttClient;
+
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        logger.debug("Server called successfully!!");
+        return new ResponseEntity<String>("Hello from server", HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @GetMapping("/question")
+    ResponseEntity<QuestionDTO> getQuestion() {
+        String content = watchService.getQuestionContent();
+        if (content == null) {
+            return new ResponseEntity<QuestionDTO>(HttpStatus.NO_CONTENT);
+        }
+        QuestionDTO dto = new QuestionDTO(content);
+        return new ResponseEntity<QuestionDTO>(dto, HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @PostMapping
+    public ResponseEntity<TokenDTO> createVoteForUser(@RequestBody TokenDTO token, HttpServletRequest request) {
+        String ipAddress = request.getRemoteAddr();
+        if (!authorizationService.checkAndAddAddress(ipAddress)) {
+            logger.info("Vote already created from host '{}'", ipAddress);
+            return new ResponseEntity<>(HttpStatus.PRECONDITION_FAILED);
+        }
+        Vote vote = voteRepository.createVote(token.getEmail());
+        if (vote == null) {
+            logger.info("Vote already created for '{}'", token.getEmail());
+            return new ResponseEntity<>(HttpStatus.PRECONDITION_FAILED);
+        }
+        TokenDTO tokenDTO = new TokenDTO(vote.getId(), token.getEmail());
+        logger.debug("Vote created for '" + vote.getEmail() + "'");
+        return new ResponseEntity<TokenDTO>(tokenDTO, HttpStatus.CREATED);
+    }
+
+    @CrossOrigin
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> vote(@PathVariable String id, @RequestBody @Valid VoteDTO dto, BindingResult result) {
+        if (result.hasErrors()) {
+            logger.error("Validation failed!");
+            return new ResponseEntity<Void>(HttpStatus.PRECONDITION_FAILED);
+        }
+        Vote vote = voteRepository.findVote(id);
+        if (vote == null) {
+            logger.error("No vote found for '{}'", id);
+            return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+        }
+        if (vote.isClosed()) {
+            logger.info("Already voted: '" + vote.getEmail() + "'");
+            return new ResponseEntity<Void>(HttpStatus.TOO_MANY_REQUESTS);
+        }
+        vote.setTrue(dto.getVote());
+        voteRepository.updateVote(vote);
+        mqttClient.publish(voteRepository.findAll());
+        logger.debug("Vote updated for '" + vote.getEmail() + "'");
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+
+}
